851d79903c4cbdbdf5cf64c434584adcae5d6a09
Jens Weil <jens.jenka@gmail.com>
2026-06-03 20:16:26 +0200

    fix: allow loading images from Supabase Storage in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'i.ytimg.com' },
       { protocol: 'https', hostname: 'i.vimeocdn.com' },
+      { protocol: 'https', hostname: '*.supabase.co' },
     ],
   },
 };
